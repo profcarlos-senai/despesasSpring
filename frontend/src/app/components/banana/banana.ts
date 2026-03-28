16ea5b13ae5f161af60664febd0f3d1c0d44f475
Carlos Correia <carlos.correia@sistemafiep.org.br>
2026-03-27 22:35:07 -0300

    SOU UMA BANANA

---
+++ frontend/src/app/components/banana/banana.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banana',
+  imports: [],
+  template: ` <h1>SOU UMA BANANA!</h1><p><a href="despesas">CLIQUE AQUI</a></p> `,
+  styles: ``,
+})
+export class BananaComponent {}
